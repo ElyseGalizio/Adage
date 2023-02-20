44f4f0005d185ba009d9f115b8353579f8337a75
Watt93 <54727031+Watt93@users.noreply.github.com>
2023-02-20 23:32:36 +0100

    translate function working but bad design

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ fetch('https://api.quotable.io/random')
     document.getElementById("author").innerHTML = quotes.author;
   })
 
+  
 
 
 // Download quote as image 
-
 // Save a 'screesnhot' of a div as an image
 function takeScreenshot(){
   html2canvas(document.querySelector("#quote-container")).then(canvas => {
@@ -119,4 +119,67 @@ button.addEventListener('click', function(e) {
 		speak(speechMsgInput.innerHTML);
 	}
 });
+
+
+
+//
+// Translate feature
+//
+
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
+
+
+// Select language
+
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "fr-FR" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+
+// Translate btn
+
+translateBtn.addEventListener("click", () => {
+    let text = speechMsgInput.innerHTML,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if(data.id === 0) {
+              document.getElementById("quote-text").innerHTML = data.translation;
+            }
+        });
+        toText.setAttribute("placeholder", "Translation");
+    });
+});
+
+// Write text
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(!fromText.value || !toText.value) return;
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } 
+    });
+});
+
+// Replace original text by French
+
 
