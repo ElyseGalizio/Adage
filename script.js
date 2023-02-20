a7a45446d03f2aaff42190d78aa40fec014b1ce2
Elyse Galizio <elyse.galizio@gmail.com>
2023-02-20 17:57:12 +0100

    implemented javascript function for navbar and hamburger menu

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ fetch('https://api.quotable.io/random')
     document.getElementById("quote").innerHTML = quotes.content;
     document.getElementById("author").innerHTML = quotes.author;
   })
+
+  function toggleNavbar() {
+    const navLinks = document.getElementsByClassName("nav-link-list");
+    if (navLinks.style.display === "none") {
+      navLinks.style.display = "block";
+    } else {
+      navLinks.style.display = "none";
+    }
+  } 
