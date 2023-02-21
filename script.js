2bb18f653391d977414e51ea31e112d3de2289f4
Elyse Galizio <elyse.galizio@gmail.com>
2023-02-21 14:11:02 +0100

    began javascript to save favorite quotes to favorite page

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 fetch('https://api.quotable.io/random')
   .then(response => response.json())
   .then(quotes => {
-    document.getElementById("quote").innerHTML = quotes.content;
-    document.getElementById("author").innerHTML = quotes.author;
+    // document.getElementById("quote").innerHTML = quotes.content;
+    // document.getElementById("author").innerHTML = quotes.author;
   })
 
+
+
+// Initialize an empty array to store quotes
+const favoriteQuotes = [];
+
+function addtoFavorites() {
   
+  const quote = document.getElementById("quote").innerText;
+  const author = document.getElementById("author").innerText;
+
+  const quoteObject = {
+    quote: quote,
+    author: author
+  }
+
+  favoriteQuotes.push(quoteObject);
+  console.log(favoriteQuotes);
+
+}
+
+function generateFavoriteQuoteCard(quote, author) {
+  document.getElementById("favorites-container").innerHTML += `<div>
+  <p class="quote">${quote}</p>
+  <h2 class="author">${author}</h2>
+  </div>`
+}
+
+function renderFavoriteQuotes() {
+  for (let i = 0; i < favoriteQuotes.length; i++) {
+    generateFavoriteQuoteCard(favoriteQuotes[i].quote, favoriteQuotes[i].author)
+  }
+  console.log(favoriteQuotes);
+}
+
+// create function to add quote to array
+// Create a function which injects each quote into favorites page
+// when favorites page loads, call function which injects each quote into favorites page
