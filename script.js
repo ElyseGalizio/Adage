29e4aba2a8d3b684e0d457eb053632d1c9881969
Elyse Galizio <elyse.galizio@gmail.com>
2023-02-22 13:25:44 +0100

    created functions to store favorite quotes in local storage, retrieve quotes, and inject into DOM

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-fetch('https://api.quotable.io/random')
+function generateQuote() {
+  fetch('https://api.quotable.io/random')
   .then(response => response.json())
   .then(quotes => {
-    // document.getElementById("quote").innerHTML = quotes.content;
-    // document.getElementById("author").innerHTML = quotes.author;
+    document.getElementById("quote").innerHTML = quotes.content;
+    document.getElementById("author").innerHTML = quotes.author;
   })
+}
 
-
-
-// Initialize an empty array to store quotes
-const favoriteQuotes = [];
 
 // create function to add quote to array
 function addtoFavorites() {
@@ -20,31 +18,36 @@ function addtoFavorites() {
     author: author
   }
 
-  favoriteQuotes.push(quoteObject);
-  console.log(favoriteQuotes);
-  
-  // store in local storage
-  
-  window.localStorage.setItem('quote', JSON.stringify(quoteObject));
-  window.localStorage.setItem('author', JSON.stringify(quoteObject));
+  const favoriteQuotes = JSON.parse(window.localStorage.getItem("quote"));
+  if (favoriteQuotes !== null) {
+    favoriteQuotes.push(quoteObject);
+    window.localStorage.setItem('quote', JSON.stringify(favoriteQuotes));
+  } else {
+    window.localStorage.setItem('quote', JSON.stringify([quoteObject]));
+  }
 }
 
+// create a function which retrieves favorites array from local storage and injects into DOM
 
-// create a function which retrieves favorites array from local storage
-function generateFavoriteQuoteCard(quote, author) {
-  window.localStorage.getItem(quote, author);
+function generateFavoriteQuoteCard(quote, author) { 
+  document.getElementById("fav-click").innerHTML = "";
   document.getElementById("favorites-container").innerHTML += `<div>
-  <p class="quote">${quote}</p>
-  <h2 class="author">${author}</h2>
+  <p class="fav-quote">${quote}</p>
+  <h2 class="fav-author">${author}</h2>
+  <hr>
   </div>`
 }
 
 function renderFavoriteQuotes() {
-  for (let i = 0; i < favoriteQuotes.length; i++) {
-    generateFavoriteQuoteCard(favoriteQuotes[i].quote, favoriteQuotes[i].author)
+  const favQuotesLocalStorage = JSON.parse(window.localStorage.getItem("quote"));
+  for (let i = 0; i < favQuotesLocalStorage.length; i++) {
+    generateFavoriteQuoteCard(favQuotesLocalStorage[i].quote, favQuotesLocalStorage[i].author)
+    console.log(favQuotesLocalStorage[i]);
   }
-  console.log(favoriteQuotes);
 }
 
-// Create a function which injects each quote into favorites page from local storage
-// when favorites page loads, call function which injects each quote into favorites page
+function clearFavorites() {
+  window.localStorage.clear();
+  document.getElementById("favorites-container").innerHTML = "";
+  document.getElementById("fav-click").innerHTML = "Click the logo to add your favorite quotes";
+}
