1c974b629f65b7412ebda74837dbf0074b366e91
Elyse Galizio <elyse.galizio@gmail.com>
2023-02-22 13:31:52 +0100

    adjusted code to have fewer conflicts

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function generateQuote() {
-  fetch('https://api.quotable.io/random')
-  .then(response => response.json())
-  .then(quotes => {
-    document.getElementById("quote").innerHTML = quotes.content;
-    document.getElementById("author").innerHTML = quotes.author;
-  })
-}
-
-
 // create function to add quote to array
 function addtoFavorites() {
   const quote = document.getElementById("quote").innerText;
